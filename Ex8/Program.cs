cb7be530e55690d6961338e731a591510a1dfc75
BuMax <max@butenko.in>
2022-11-27 23:41:07 +1000

    Exercise #8 changed

--- Ex8/Program.cs
+++ Ex8/Program.cs
@@ -1,4 +1,26 @@
-﻿Console.Write("Введите число: ");
+﻿
+// Вариант с перебором в отрицательную сторону
+Console.Write("Введите число: ");
+var number = Convert.ToInt32(Console.ReadLine());
+
+bool negativeFlag = new bool();
+
+if (number > 0)
+    for(int i = 1; i <= number; i++){
+    if(i%2 == 0)
+        Console.WriteLine(i);
+}
+else
+{
+    for(int i = 1; i >= number; i--){
+    if(i%2 == 0)
+        Console.WriteLine(i * (1 - (negativeFlag ? 1 : 0) * 2));
+}
+}
+
+/*
+// Обычный вариант
+Console.Write("Введите число: ");
 var number = Convert.ToInt32(Console.ReadLine());
 
 
@@ -6,3 +28,4 @@ for(int i = 1; i <= number; i++){
     if(i%2 == 0)
         Console.WriteLine(i);
 }
+*/
