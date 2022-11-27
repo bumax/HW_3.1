cbe7b1de2d9d80b57946859f523e18fc6d2e3fcc
BuMax <max@butenko.in>
2022-11-27 23:13:37 +1000

    Exercise #6 completed

---
+++ Ex6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+var number = Convert.ToInt32(Console.ReadLine());
+
+
+// вместо остатка от деления, можно проверить нулевой бит у числа, если он присутствует, то число нечетное
+Console.Write("{0} - это число ", number);
+Console.WriteLine(((number & 0b1) == 1 ? "нечетное" : "четное"));
