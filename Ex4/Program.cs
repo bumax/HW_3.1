0fc223286f4b6d0f68fc3c68e1d9ca0cd75a6801
BuMax <max@butenko.in>
2022-11-27 23:05:54 +1000

    Exercise #4 completed

---
+++ Ex4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+int[] numbers = new int[3];
+Console.Write("Введите число 1: ");
+numbers[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+numbers[1] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+numbers[2] = Convert.ToInt32(Console.ReadLine());
+
+// мы не ищем легких путей :-)
+Array.Sort(numbers);
+
+System.Console.WriteLine("Максимальное число - {0}", numbers[2]);
